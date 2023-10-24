a59316e6cc51f54896ebdfce2d0161e2f3965ad1
Kenny <kce6@njit.edu>
2023-10-24 00:36:24 +0000

    added absolute paths to count file

--- shifarbot_responses.py
+++ shifarbot_responses.py
@@ -20,18 +20,18 @@ shifar_quotes = [
 
 # function for getting current disconnect count
 def get_total_dc() -> int:
-	f = open("shifarbot_dccount.txt", "r")
+	f = open("/opt/bots/shifarbot/shifarbot_dccount.txt", "r")
 	total_dc = f.read()
 	f.close()
 	return total_dc
 
 # function for updating disconnect count and writing to file
 def update_total_dc() -> int:
-	f = open("shifarbot_dccount.txt", "r")
+	f = open("/opt/bots/shifarbot/shifarbot_dccount.txt", "r")
 	current_dc = f.read()
 	new_dc = int(current_dc) + 1
 	new_dc = str(new_dc)
-	f = open("shifarbot_dccount.txt", "w")
+	f = open("/opt/bots/shifarbot/shifarbot_dccount.txt", "w")
 	f.write(new_dc)
 	f.close()
 
